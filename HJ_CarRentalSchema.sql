b251f6dbb212395780bfaf1fc3c4915101500b45
HJohnson748 <hannahjohnson748@u.boisestate.edu>
2019-04-29 14:39:51 -0600

    Changed database schema. Moved type of cars into car table

--- HJ_CarRentalSchema.sql
+++ HJ_CarRentalSchema.sql
@@ -14,48 +14,15 @@ CREATE TABLE car (
 	plate_number varchar(10) not null,
     num_miles int not null,
     status varchar(10) not null,    
-    model int not null,
+    model int not null,    
+    type varchar(15) not null,
+    fee int,
     primary key (plate_number),
     foreign key (model) REFERENCES model(model_id),
-    CHECK (status='Rented' OR status='Available' OR status='Servicing')
+    CHECK (status='Rented' OR status='Available' OR status='Servicing'),
+    CHECK (type='Economy' OR type='Compact' OR type='Midsize' OR type='Fullsize' OR type='Premium' OR type='Luxury')
 );
 
-
-CREATE TABLE economy (
-	plate_number varchar(10) not null,
-	fee int not null,
-    foreign key (plate_number) references car(plate_number)
-);
-
-CREATE TABLE compact (
-	plate_number varchar(10) not null,
-	fee int not null,
-    foreign key (plate_number) references car(plate_number)
-);
-
-CREATE TABLE midsize (
-	plate_number varchar(10) not null,
-	fee int not null,
-    foreign key (plate_number) references car(plate_number)
-);
-
-CREATE TABLE fullsize (
-	plate_number varchar(10) not null,
-	fee int not null,
-    foreign key (plate_number) references car(plate_number)
-);
-
-CREATE TABLE premium (
-	plate_number varchar(10) not null,
-	fee int not null,
-    foreign key (plate_number) references car(plate_number)
-);
-
-CREATE TABLE luxury (
-	plate_number varchar(10) not null,
-	fee int not null,
-    foreign key (plate_number) references car(plate_number)
-);
 
 CREATE TABLE client (
 	code int not null auto_increment,
@@ -66,14 +33,15 @@ CREATE TABLE client (
 );
 
 CREATE TABLE rent (
+	rental_id int not null auto_increment,
 	plate varchar(10) not null,
     code int not null,
     start date not null,
     end date not null,
     num_miles int,
     fee_type varchar(10) not null,
+    primary key (rental_id),
     foreign key (plate) REFERENCES car(plate_number),
-    foreign key (code) references client(code),
-    CHECK (datediff(start, end) <> 0)
+    foreign key (code) references client(code)
 );
 
--- HJ_PopulateCarRental.sql
+++ HJ_PopulateCarRental.sql
@@ -32,63 +32,38 @@ INSERT INTO model values (0, 'Mustang', 'Ford', '2.3', '2018');
 
 
 /*CARS ADDED TO POOL */
-INSERT INTO car values ('1A12345', 15000, 'Available', 1);
-INSERT INTO car values ('1A54321', 15000, 'Available', 2);
-INSERT INTO car values ('1AB4123', 15000, 'Available', 3);
-INSERT INTO car values ('1AB4321', 15000, 'Available', 4);
-INSERT INTO car values ('1A3E914', 15000, 'Available', 5);
-INSERT INTO car values ('1A9S931', 15000, 'Available', 6);
-INSERT INTO car values ('1A02935', 15000, 'Available', 7);
-INSERT INTO car values ('1AD0193', 15000, 'Available', 8);
-INSERT INTO car values ('1AN0392', 15000, 'Available', 9);
-INSERT INTO car values ('1AS1834', 15000, 'Available', 10);
-INSERT INTO car values ('1AP8492', 15000, 'Rented', 11);
-INSERT INTO car values ('1AW9301', 15000, 'Servicing', 12);
-INSERT INTO car values ('1A492D1', 15000, 'Available', 13);
-INSERT INTO car values ('1ACS910', 15000, 'Available', 14);
-INSERT INTO car values ('1AOE934', 15000, 'Available', 15);
-INSERT INTO car values ('1AR940S2', 15000, 'Available', 16);
-INSERT INTO car values ('1AN3V91', 15000, 'Available', 17);
-INSERT INTO car values ('1APW961', 15000, 'Available', 18);
-INSERT INTO car values ('1AW9320', 15000, 'Available', 19);
-INSERT INTO car values ('1A9402S', 15000, 'Available', 20);
+INSERT INTO car values ('1A12345', 15000, 'Available', 1, 'Midsize', 75);
+INSERT INTO car values ('1A54321', 15000, 'Available', 2, 'Midsize', 75);
+INSERT INTO car values ('1AB4123', 15000, 'Available', 3, 'Midsize', 75);
+INSERT INTO car values ('1AB4321', 15000, 'Available', 4, 'Fullsize', 100);
+INSERT INTO car values ('1A3E914', 15000, 'Available', 5, 'Luxury', 200);
+INSERT INTO car values ('1A9S931', 15000, 'Available', 6, 'Luxury', 200);
+INSERT INTO car values ('1A02935', 15000, 'Available', 7, 'Luxury', 200);
+INSERT INTO car values ('1AD0193', 15000, 'Available', 8, 'Luxury', 200);
+INSERT INTO car values ('1AN0392', 15000, 'Available', 9, 'Economy', 50);
+INSERT INTO car values ('1AS1834', 15000, 'Available', 10, 'Economy', 50);
+INSERT INTO car values ('1AP8492', 15000, 'Rented', 11, 'Compact', 65);
+INSERT INTO car values ('1AW9301', 15000, 'Servicing', 12, 'Compact', 65);
+INSERT INTO car values ('1A492D1', 15000, 'Available', 13, 'Fullsize', 100);
+INSERT INTO car values ('1ACS910', 15000, 'Available', 14, 'Fullsize', 100);
+INSERT INTO car values ('1AOE934', 15000, 'Available', 15, 'Economy', 50);
+INSERT INTO car values ('1AR90S2', 15000, 'Available', 16, 'Compact', 65);
+INSERT INTO car values ('1AN3V91', 15000, 'Available', 17, 'Premium', 150);
+INSERT INTO car values ('1APW961', 15000, 'Available', 18, 'Premium', 150);
+INSERT INTO car values ('1AW9320', 15000, 'Available', 19, 'Premium', 150);
+INSERT INTO car values ('1A9402S', 15000, 'Available', 20, 'Luxury', 200);
 
-/* ADD CARS TO CORRECT CATEGORIES*/
-INSERT INTO economy values ('1AN0392', 50);
-INSERT INTO economy values ('1AS1834', 50);
-INSERT INTO economy values ('1AOE934', 50);
 
-INSERT INTO compact values ('1AP8492', 65);
-INSERT INTO compact values ('1AW9301', 65);
-INSERT INTO compact values ('1AR940S2', 65);
-
-INSERT INTO midsize values ('1A12345', 75);
-INSERT INTO midsize values ('1A54321', 75);
-INSERT INTO midsize values ('1AB4123', 75);
-
-INSERT INTO fullsize values ('1AB4321', 100);
-INSERT INTO fullsize values ('1A492D1', 100);
-INSERT INTO fullsize values ('1ACS910', 100);
-
-INSERT INTO premium values ('1AN3V91', 150);
-INSERT INTO premium values ('1APW961', 150);
-INSERT INTO premium values ('1AW9320', 150);
-
-INSERT INTO luxury values ('1A3E914', 200);
-INSERT INTO luxury values ('1A9S931', 200);
-INSERT INTO luxury values ('1A02935', 200);
-INSERT INTO luxury values ('1AD0193', 200);
-INSERT INTO luxury values ('1A9402S', 200);
-
-/* ADD SOME CLIENTS */
+/* ADD SOME CLIENTS */ 
 INSERT INTO client values (0, 'John Doe', 'RD829518E', '2087461832');
 INSERT INTO client values (0, 'Jane Doe', 'CS748195D', '2081234567');
 INSERT INTO client values (0, 'John Smith', 'LS857281P', '2089369023');
 INSERT INTO client values (0, 'Jane Johnson', 'SP395287V', '2089492132');
 
+
 /* ADD SAMPLE RENTALS */
-INSERT INTO rent values ('1A02935', 1, '2019-04-21', '2019-04-21', 1000, 'economy');
-INSERT INTO rent values ('1AP8492', 4, '2019-04-21', '2019-04-26', 1500, 'premium');
+INSERT INTO rent values (0, '1A02935', 1, '2019-04-21', '2019-04-21', 1000, 'luxury');
+INSERT INTO rent values (0, '1AP8492', 4, '2019-04-21', '2019-04-26', 1500, 'compact');
 
 
 
